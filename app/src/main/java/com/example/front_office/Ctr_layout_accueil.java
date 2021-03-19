b217394fad8d10d8290a903a63144f772ffb4145
julien-guilet <68598612+julien-guilet@users.noreply.github.com>
2021-03-19 16:15:15 +0100

    creation des ctr

---
+++ app/src/main/java/com/example/front_office/Ctr_layout_accueil.java
@@ -0,0 +1,19 @@
+package com.example.front_office;
+
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+public class Ctr_layout_accueil extends AppCompatActivity {
+
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+
+
+
+    }
+}
---
+++ app/src/main/java/com/example/front_office/Ctr_layout_prospect.java
@@ -0,0 +1,18 @@
+package com.example.front_office;
+
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+public class Ctr_layout_prospect extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+
+
+
+    }
+}
