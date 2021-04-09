48396945d07774d9dc0b78a5a9ab4904c9f137bc
julien-guilet <68598612+julien-guilet@users.noreply.github.com>
2021-04-09 17:49:09 +0200

    Ajout du tableau

--- app/src/main/java/com/example/front_office/Ctr_layout_visiteurs.java
+++ app/src/main/java/com/example/front_office/Ctr_layout_visiteurs.java
@@ -1,8 +1,14 @@
 package com.example.front_office;
 
+import android.database.MatrixCursor;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
+import android.widget.ListView;
+import android.widget.SimpleCursorAdapter;
+import android.widget.TableLayout;
+import android.widget.TableRow;
+import android.widget.TextView;
 
 import androidx.appcompat.app.AppCompatActivity;
 
@@ -11,6 +17,31 @@ public class Ctr_layout_visiteurs extends AppCompatActivity {
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+        setContentView(R.layout.layout_visiteurs);
+        String[] columns = new String[] { "_id", "Nom", "Prenom", "Entreprise" };
+
+        // Définition des données du tableau
+        // les lignes ci-dessous ont pour seul but de simuler
+        // un objet de type Cursor pour le passer au SimpleCursorAdapter.
+        // Si vos données sont issues d'une base SQLite,
+        // utilisez votre "cursor" au lieu du "matrixCursor"
+        MatrixCursor matrixCursor= new MatrixCursor(columns);
+        startManagingCursor(matrixCursor);
+        matrixCursor.addRow(new Object[] { 1,"BERARD","Lucas", "Singes" });
+        matrixCursor.addRow(new Object[] { 2,"ARCHIER","TOITOINE", "Bouffon" });
+
+        // on prendra les données des colonnes 1 et 2...
+        String[] from = new String[] {"Nom", "Prenom", "Entreprise"};
+
+        // ...pour les placer dans les TextView définis dans "row_item.xml"
+        int[] to = new int[] { R.id.textViewCol1, R.id.textViewCol2, R.id.textViewCol3};
+
+        // création de l'objet SimpleCursorAdapter...
+        SimpleCursorAdapter adapter = new SimpleCursorAdapter(this, R.layout.row_listview, matrixCursor, from, to, 0);
+
+        // ...qui va remplir l'objet ListView
+        ListView lv = (ListView) findViewById(R.id.lv);
+        lv.setAdapter(adapter);
 
 
 
